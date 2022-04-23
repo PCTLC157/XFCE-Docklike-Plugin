534931519a29584b62493f6e550b4ea471de6f2d
PCTLC157 <84652879+PCTLC157@users.noreply.github.com>
2022-04-23 09:04:17 +0800

    Add files via upload
    This script will download, compile with the necessary dependencies and install the XFCE Docklike Plugin Taskbar. This has been tested on Peppermint which is now based on Debian, which I suppose would also work with Debian 11 with the XFCE desktop environment. I have not tested with any Ubuntu based XFCE distros.

---
+++ xfce4-docklike-plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~/Downloads
+sudo apt update && sudo apt upgrade
+sudo apt install wget xorg-dev libglib2.0-cil-dev golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev intltool
+wget https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-0.4.0.tar.bz2
+tar -xvjf xfce4-docklike-plugin-0.4.0.tar.bz2 && cd xfce4-docklike-plugin-0.4.0
+./configure
+make
+sudo make install
+
+
